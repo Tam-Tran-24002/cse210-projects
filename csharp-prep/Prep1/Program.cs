3fd2f73b51fd32c778f076a77e70362540ae70cc
Tam-Tran-24002 <tra24002@byui.edu>
2026-09-17 21:58:03 -0600

    Adding Name - quiz

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,15 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    {   
+        string firstName;
+        string lastName;
+        Console.WriteLine("Please enter your first name: ");
+        firstName = Console.ReadLine();
+        Console.WriteLine("Hey Tam,");
+        Console.WriteLine("Please enter your last name: ");
+        lastName = Console.ReadLine();
+        Console.WriteLine($"Hey {firstName} {lastName}, welcome to the program!");
+
     }
 }
