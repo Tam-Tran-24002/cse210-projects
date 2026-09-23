95ebe94b37cde16bef31ca1810ad31b6cec686e5
Tam-Tran-24002 <tra24002@byui.edu>
2026-09-22 21:35:43 -0600

    prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        Console.WriteLine("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        if (grade >= 90)
+        {
+       letter = "A";
+        }
+        else if (grade >= 80)
+        {   
+        letter = "B";
+        }
+        else if (grade >= 70)
+        {   
+        letter = "C";
+        }
+        else if (grade >= 60)
+        {   
+        letter = "D";
+        }
+       else
+        {
+        letter = "F";
+        }
+            Console.WriteLine($"Your grade is {letter}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying, you'll get it next time!");
+        }
     }
 }
